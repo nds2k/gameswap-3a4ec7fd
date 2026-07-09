f3058347e2b0e11e5a35e33e67dc608cb3095ea4
nds2k <ayox.prime@gmail.com>
2026-07-09 12:51:11 +0200

    fiox

--- src/components/games/GameDetailModal.tsx
+++ src/components/games/GameDetailModal.tsx
@@ -4,7 +4,7 @@ import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/contexts/AuthContext";
 import { supabase } from "@/integrations/supabase/client";
-// import { useWishlist } from "@/hooks/useWishlist";
+import { useWishlist } from "@/hooks/useWishlist";
 import { useNavigate } from "react-router-dom";
 import { useMessages } from "@/hooks/useMessages";
 import { useToast } from "@/hooks/use-toast";
